971ba13e10e69f5729ba4c9d890a4a27b8701376
matthewmonolith <matthew.page2016@outlook.com>
2024-07-19 15:07:30 +0100

    google oauth sorted

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,32 @@
 const express = require("express");
-const passport = require('passport')
+const passport = require("passport");
 import dotenv from "dotenv";
 dotenv.config();
-const GoogleStrategy = require('passport-google-oauth20').Strategy
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLECLIENTID,
-  clientSecret: process.env.GOOGLECLIENTSECRET,
-  callbackURL: '/auth/google/callback'
-}, (accessToken) => {
-  
-}))
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLECLIENTID,
+      clientSecret: process.env.GOOGLECLIENTSECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken, refreshToken, profile, done);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google")); //code is in this route
 
 const PORT = process.env.port || 3000;
 
