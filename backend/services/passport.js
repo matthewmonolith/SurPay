e1e61a517417ee6607449dd5905daf5cc3f4b6f3
matthewmonolith <matthew.page2016@outlook.com>
2024-07-22 14:29:17 +0100

    small changes

--- backend/services/passport.js
+++ backend/services/passport.js
@@ -14,7 +14,15 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save()
+      User.findOne({ googleId: profile.id }).then((user) => {
+        if (user) {
+          done(null, user); //arrow function to communicate to passport if there's an error, first arg null means no error/it's fine, second arg is the found user, passport uses for auth furtherance
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then((user) => done(null, user)); //end up with two model instances of user, both representing the same record in the DB collection, make use of the one from the promise callback as it might have changed when the first one was being saved
+        }
+      });
     }
   )
 );
