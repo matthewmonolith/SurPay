a03355ee81034a404a622deb06d1547669c8483e
matthewmonolith <matthew.page2016@outlook.com>
2024-08-22 14:31:02 +0100

    subdoc set up

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const bodyParser = require("body-parser");
 const keys = require("../config/dev");
 require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 //Initialisation
